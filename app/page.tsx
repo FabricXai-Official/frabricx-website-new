7ba161f216cb3e83086c6b66568f409a820a2acd
unknown <sudipta.2304@gmail.com>
2025-07-12 07:54:15 -0700

    Added challenges icons and adjusted colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/Hero";
+import SmartTools from "@/components/SmartTools";
+import SolutionsAndChallenges from "@/components/SolutionsAndChallenges";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
+      <SmartTools />
+      <SolutionsAndChallenges />
     </main>
   );
 }
